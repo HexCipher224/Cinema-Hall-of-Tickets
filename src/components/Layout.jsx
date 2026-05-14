ea9d92d0d4e6440b8b91a0a62a030f6445a9aab6
oliver moosberger <olivermooz@gmail.com>
2026-05-14 16:43:57 +0300

    layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+Layout.jsx 
+import NavigationBar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <NavigationBar />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
